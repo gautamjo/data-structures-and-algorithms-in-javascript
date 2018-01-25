f7d7858862cebd151df59b37b25198138f9677fc
gigaelectron <gautam_joshi01@yahoo.com>
2018-01-25 21:23:47 +0530

    binary search tree completed

--- binarySearchTree/binarySearchTree.js
+++ binarySearchTree/binarySearchTree.js
@@ -37,21 +37,80 @@ BST.prototype.contains = function(value) {
             return this.right.contains(value);
         }
     }
+};
+
+// in order, pre order, post order
+BST.prototype.depthFirstTraversal = function(iteratorFunc, order) {
+    if (order === "pre-order") {
+        iteratorFunc(this.value);
+    }
+    if (this.left) {
+        this.left.depthFirstTraversal(iteratorFunc, order);
+    }
+    if (order === "in-order") {
+        iteratorFunc(this.value);
+    }
+    if (this.right) {
+        this.right.depthFirstTraversal(iteratorFunc, order);
+    }
+    if (order === "post-order") {
+        iteratorFunc(this.value);
+    }
+};
+
+BST.prototype.breadthFirstTraversal = function(iteratorFunc) {
+    // queue = first in first out
+    var queue = [this];
+
+    while (queue.length) {
+        var treeNode = queue.shift();
+        iteratorFunc(treeNode);
+        if (treeNode.left) {
+            queue.push(treeNode.left);
+        }
+        if (treeNode.right) {
+            queue.push(treeNode.right);
+        }
+    }
+};
+
+BST.prototype.getMinVal = function() {
+    if (!this.left) {
+        return this.value;
+    }
+    return this.left.getMinVal();
+};
+
+BST.prototype.getMaxVal = function() {
+    if (!this.right) {
+        return this.value;
+    }
+    return this.right.getMaxVal();
+};
+
+function logDepth(value) {
+    console.log(value);
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
+function logBreadth(node) {
+    console.log(node.value);
+}
 
 var bst = new BST(50);
+
 bst.insert(20);
-console.log(bst);
+bst.insert(30);
+bst.insert(15);
+bst.insert(70);
+bst.insert(65);
+bst.insert(75);
+console.log("Node Value", bst.value);
+console.log("\n");
+console.log("Post-order depth first traversal");
+bst.depthFirstTraversal(logDepth, "post-order");
+console.log("\n");
+console.log("breadth first traversal");
+bst.breadthFirstTraversal(logBreadth);
+console.log("\n");
+console.log("minimum value:", bst.getMinVal());
+console.log("maximum value:", bst.getMaxVal());
