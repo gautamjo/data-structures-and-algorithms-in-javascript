578582e6951b3cd75f5c094a055fdc99b298fbf0
gigaelectron <gautam_joshi01@yahoo.com>
2018-01-25 13:58:37 +0530

    binary search tree correction

--- binarySearchTree/binarySearchTree.js
+++ binarySearchTree/binarySearchTree.js
@@ -1,9 +1,57 @@
 function BST(value) {
-	this.value = value;
-	this.leftNode = null;
-	this.rightNode = null;
+    this.value = value;
+    this.left = null;
+    this.right = null;
 }
 
-var bst = new BST(50);
+BST.prototype.insert = function(value) {
+    if (value <= this.value) {
+        if (!this.left) {
+            this.left = new BST(value);
+        } else {
+            this.left.insert(value);
+        }
+    } else if (value > this.value) {
+        if (!this.right) {
+            this.right = new BST(value);
+        } else {
+            this.right.insert(value);
+        }
 
+    }
+};
+
+BST.prototype.contains = function(value) {
+    if (value === this.value) {
+        return true;
+    } else if (value < this.value) {
+        if (!this.left) {
+            return false;
+        } else {
+            return this.left.contains(value);
+        }
+    } else if (value > this.value) {
+        if (!this.right) {
+            return false;
+        } else {
+            return this.right.contains(value);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var bst = new BST(50);
+bst.insert(20);
 console.log(bst);
